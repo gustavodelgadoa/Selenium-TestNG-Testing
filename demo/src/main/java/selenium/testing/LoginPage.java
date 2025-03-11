e035ca35221cfc152810f4913d9cb4aab892787a
Gustavo Delgado <gustavodelgadoa@outlook.com>
2025-03-10 21:47:25 -0400

    Saving current work.

--- demo/src/main/java/selenium/testing/LoginPage.java
+++ demo/src/main/java/selenium/testing/LoginPage.java
@@ -1,8 +1,39 @@
 package selenium.testing;
 
+import org.openqa.selenium.By;
+
 /**
  * 
+ * Extends BasePage so it can use its methods.
  */
 public class LoginPage extends BasePage {
+    // Instance Variables
+    private By usernameField = By.name("user-name");
+    private By passwordField = By.name("password");
+    private By loginButton = By.name("login-button");
+    private By errorMessage = By.tagName("h3");
+
+    /**
+     * Convenience method, performs all login actions.
+     * 
+     * @param username First String of text that will be passed in & entered in
+     *                 username input field.
+     * @param password Second String of text that will be passed in & entered in
+     *                 password input field.
+     */
+    public void login(String username, String password) {
+        set(usernameField, username);
+        set(passwordField, password);
+        click(loginButton);
+    } // login
+
+    /**
+     * Method returns the error message element's text.
+     * 
+     * @return returns error message in text format.
+     */
+    public String getErrorMessage() {
+        return find(errorMessage).getText();
+    } // getErrorMessage
 
 } // LoginPage
--- demo/src/main/java/selenium/testing/ProductsPage.java
+++ demo/src/main/java/selenium/testing/ProductsPage.java
@@ -1,8 +1,18 @@
 package selenium.testing;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
  * 
  */
 public class ProductsPage extends BasePage{
 
-}
+    // Instance Variables
+    private By productsLogo = By.cssSelector("span.title");
+
+    // Finding web element and creating actual & expected string variables.
+    WebElement productsLogoText = find(productsLogo);
+    String actualResult = productsLogoText.getText();
+    String expectedResult = "Products";
+} // ProductsPage
